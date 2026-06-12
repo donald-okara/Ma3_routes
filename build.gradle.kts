f5eef5a3608121756da2f394cd4cafb1dc88f5e6
donald-okara <donaldokara123@gmail.com>
2026-06-12 12:27:23 +0300

    Add Apache License 2.0 and enforce license headers via Spotless
    - Add the project-wide `LICENSE` file (Apache License 2.0).
    - Create `spotless/copyright.txt` to serve as the template for source file headers.
    - Configure the Spotless plugin in the root `build.gradle.kts` to automate license header injection for Kotlin files and Gradle scripts.
    - Apply the license header to all existing source files across the `app`, `core`, `features`, `datasources`, and `build-logic` modules.
    - Simplify repository declarations in `settings.gradle.kts` by removing specific group regex filters.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,18 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
+/*
+ * Copyright 2025 Donald Isoe
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.hilt.android) apply false
@@ -17,12 +31,17 @@ spotless {
                 "ktlint_standard_function-naming" to "disabled",
             ),
         )
+        licenseHeaderFile(rootProject.file("spotless/copyright.txt"))
     }
 
     kotlinGradle {
         target("**/*.gradle.kts")
         targetExclude("**/build/**")
         ktlint(libs.versions.ktlint.get())
+        licenseHeaderFile(
+            rootProject.file("spotless/copyright.txt"),
+            "(pluginManagement |plugins |import |@|apply |dependencyResolutionManagement |rootProject )",
+        )
     }
 
     format("misc") {
