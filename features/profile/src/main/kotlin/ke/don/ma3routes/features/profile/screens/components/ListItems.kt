3a3fc08a99fca8e7b5ea8f1ca9c78ef989d7c77c
donald-okara <donaldokara123@gmail.com>
2026-06-20 15:20:41 +0300

    add list item

--- features/profile/src/main/kotlin/ke/don/ma3routes/features/profile/screens/components/ListItems.kt
+++ features/profile/src/main/kotlin/ke/don/ma3routes/features/profile/screens/components/ListItems.kt
@@ -1,18 +1,25 @@
 package ke.don.ma3routes.features.profile.screens.components
 
+import android.graphics.pdf.models.ListItem
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.outlined.AddAlert
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import ke.don.ma3routes.core.ui.theme.preview.Ma3PreviewLightDark
@@ -22,8 +29,7 @@ import ke.don.ma3routes.core.ui.theme.preview.PreviewContent
 internal fun ListSegment(
     title: String,
     modifier: Modifier = Modifier,
-    content: @Composable ColumnScope.() -> Unit,
-
+    content: @Composable ColumnScope.() -> Unit
 ){
     Column(
         horizontalAlignment = Alignment.Start,
@@ -40,6 +46,50 @@ internal fun ListSegment(
     }
 }
 
+@Composable
+internal fun ListItem(
+    icon: ImageVector,
+    title: String,
+    modifier: Modifier = Modifier,
+    bottomComponent: (@Composable () -> Unit)? = null
+) {
+    Column(
+        modifier = modifier.fillMaxWidth(),
+        verticalArrangement = Arrangement.spacedBy(24.dp, Alignment.CenterVertically),
+        horizontalAlignment = Alignment.Start
+    ) {
+        Row(
+            horizontalArrangement = Arrangement.spacedBy(16.dp, Alignment.Start),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Icon(
+                imageVector = icon,
+                contentDescription = null
+            )
+
+            Text(
+                text = title,
+                style = MaterialTheme.typography.titleMedium,
+            )
+        }
+
+        bottomComponent?.invoke()
+    }
+}
+
+@Ma3PreviewLightDark
+@Composable
+fun ListItemPreview(){
+    PreviewContent {
+        ListItem(
+            icon = Icons.Outlined.AddAlert,
+            title = "Notifications"
+        ){
+            PlaceholderComponent()
+        }
+    }
+}
+
 @Ma3PreviewLightDark
 @Composable
 fun ListSegmentPreview(){
@@ -47,19 +97,24 @@ fun ListSegmentPreview(){
         ListSegment(
             title = "Preview"
         ){
-            Box(
-                contentAlignment = Alignment.Center,
-                modifier = Modifier
-                    .clip(MaterialTheme.shapes.medium)
-                    .background(MaterialTheme.colorScheme.primaryContainer)
-                    .fillMaxWidth()
-            ){
-                Text(
-                    text = "Preview Content",
-                    modifier = Modifier.padding(16.dp),
-                    color = MaterialTheme.colorScheme.onPrimaryContainer
-                )
-            }
+            PlaceholderComponent()
         }
+    }
+}
+
+@Composable
+private fun PlaceholderComponent() {
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier
+            .clip(MaterialTheme.shapes.medium)
+            .background(MaterialTheme.colorScheme.primaryContainer)
+            .fillMaxWidth(),
+    ) {
+        Text(
+            text = "Preview Content",
+            modifier = Modifier.padding(16.dp),
+            color = MaterialTheme.colorScheme.onPrimaryContainer,
+        )
     }
 }
